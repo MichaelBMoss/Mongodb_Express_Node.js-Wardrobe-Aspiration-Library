e1dd1d861e7181e0c7d6671bc09e3b4c2f5e4e2b
MichaelBMoss <michaelbmoss@gmail.com>
2023-11-02 08:56:08 -0700

    different profile for user and others working

--- controllers/profile.js
+++ controllers/profile.js
@@ -3,5 +3,25 @@ module.exports = {
   };
 
 function profile(req, res) {
-res.render('profile/profile', { title: 'My Profile', });
+    if (req.isAuthenticated()) {
+      const authenticatedUserId = req.user.googleId;
+      const userIdBeingViewed = req.params.googleId;      
+  
+      if (isUserMatching(authenticatedUserId, userIdBeingViewed)) {
+        // Render the user's own profile page
+        res.render('profile/myProfile', { title: 'My Profile' });
+      } else {
+        // Render another user's profile or handle other cases as needed
+        console.log('mmmmmmmmmmmmmmmmmmmmmm')
+        res.render('profile/profile', { title: 'Profile' });
+      }
+    } else { 
+      // User is not authenticated, you can handle this case, e.g., redirect to login
+      res.render('profile/profile', { title: 'My Profile' });
+    }
+}
+  
+  // Function to check if the user matches the page they are viewing
+function isUserMatching(authenticatedUserId, userIdBeingViewed) {
+    return authenticatedUserId === userIdBeingViewed;
 }
--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-// You'll be creating this controller module next
+
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 	
 
 const profileCtrl = require('../controllers/profile');
 
-router.get('/:googleID', profileCtrl.profile);
+router.get('/:googleId', profileCtrl.profile);
 
 module.exports = router;
